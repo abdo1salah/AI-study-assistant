c13173a5569ad9333d6321b1371a8e46642d1ce9
abdo1salah <100434550+abdo1salah@users.noreply.github.com>
2026-01-21 11:28:15 +0200

    Delete test.py

--- test.py
+++
@@ -1,3 +0,0 @@
-from generate_response import generate_response
-print("--- Testing generate_response ---")
-print(generate_response("Operating Systems", "What is a deadlock in operating systems?"))
